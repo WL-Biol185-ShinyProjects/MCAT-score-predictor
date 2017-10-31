9b1d3b56f2be369d2464a795dad8a66f7fa33364
Ron Perets <peretsr18@mail.wlu.edu>
2017-10-31 13:12:00 -0400

    Attempt at writing function for text output

--- score predictor.R
+++ score predictor.R
@@ -1,11 +1,22 @@
 
 practiceScorePredictor <- function(examType)
+  
+{
+  CPtest <- paste0(exam, ".CP")
   filterTablecp <- MCAT_clean_data %>%
-    filter(examType.CP == input$cpScore)
-  filterTableCARS <- MCAT_clean_data %>%
-    filter(examType.CARS == input$carsScore)
+    filter(CPtest == input$cpScore)
+ 
+  carstest <- paste0(exam, ".CARS")
+  filterTablecars<- MCAT_clean_data %>%
+    filter(carstest == input$carsScore)
+  
+  bbtest <- paste0(exam, ".BB")
   filterTablebb <- MCAT_clean_data %>%
-    filter(examType.BB == input$bbScore)
+    filter(bbtest == input$bbScore)
+  
+  pstest <- paste0(exam, ".PS")
   filterTableps <- MCAT_clean_data %>%
-    filter(examType.PS == input$psScore)
+    filter(pstest == input$psScore)
   median(filterTablecp$Real.CP) + median(filterTableCARS$Real.CARS) + median(filterTablebb$Real.BB) + median(filterTableps$Real.PS)
+  
+  }
